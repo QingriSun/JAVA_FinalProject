df4e03e08ff8d8dadc89ae312cba49558780b223
79885 <7988501@qq.com>
2025-05-12 21:04:58 +0800

    initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import model.MapModel;
+import view.game.GameFrame;
+import view.login.LoginFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame loginFrame = new LoginFrame(280, 280);
+            loginFrame.setVisible(true);
+        });
+    }
+}
